66be7b157c4eccbed120af9f333811dd6857c50d
tales1982 <tales_lima_1982@hotmail.com>
2023-05-26 22:03:18 +0200

    function

---
+++ Aula-05-funcao/index.js
@@ -0,0 +1,12 @@
+"use strict";
+/*
+Tipo de parametro de funcao
+podemos definir o tipo de cada parametro de uma funcao;
+Assin condicionamos o seu uso correto;
+A sintaxe é functio minhafucao(nome:string){}
+Agora podemos passar o parametro nome, apenas como texto;
+*/
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(5, 9));
