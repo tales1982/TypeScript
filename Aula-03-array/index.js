6f1f04da9a5021f98795c11d9626429958ba647d
tales1982 <tales_lima_1982@hotmail.com>
2023-05-26 21:26:34 +0200

    Arrays

---
+++ Aula-03-array/index.js
@@ -0,0 +1,18 @@
+"use strict";
+// ARRAYS NUMBERS
+let num = [11, 22, 44];
+num.push(55); //ajust one iten on last place
+console.log(num);
+console.log(num[2]);
+num.pop(); //remove the last element
+console.log(num);
+//ARRAY STRING
+let text = ["Tales", "ALEX", "Theo", "Thomas"];
+console.log(text);
+text.push("Adria");
+console.log(text);
+//Outra sistaxe de arrays 
+let num2 = [100, 200, 300];
+console.log(num2);
+num2.push(5);
+console.log(num2);
---
+++ Aula-03-array/index.ts
@@ -0,0 +1,23 @@
+// ARRAYS NUMBERS
+let num:number[]=[11,22,44];
+num.push(55);//ajust one iten on last place
+console.log(num);
+console.log(num[2]);
+num.pop();//remove the last element
+console.log(num);
+
+
+//ARRAY STRING
+
+let text:string[] = ["Tales","ALEX","Theo","Thomas"];
+console.log(text);
+
+text.push("Adria");
+console.log(text);
+
+//Outra sistaxe de arrays 
+
+let num2: Array<number> = [100, 200, 300];
+console.log(num2);
+num2.push(5);
+console.log(num2)
