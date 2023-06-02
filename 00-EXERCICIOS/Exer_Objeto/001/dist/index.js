3f211ba0998554e6ee4016b01408e7904e68f0a9
tales1982 <tales_lima_1982@hotmail.com>
2023-06-02 23:59:46 +0200

    Exercicio Formulario

--- 00-EXERCICIOS/Exer_Objeto/001/dist/index.js
+++ 00-EXERCICIOS/Exer_Objeto/001/dist/index.js
@@ -8,12 +8,13 @@ var Maquina = /** @class */ (function () {
     }
     Maquina.prototype.apresentar = function () {
         var paragrafo = document.getElementById("result");
-        paragrafo.innerHTML = "Nome..: ".concat(this.nome, " -->\n  Modelo..: ").concat(this.modelo, " -->\n  Serie...: ").concat(this.serie, " -->\n  Maquinista..: ").concat(this.maquinista);
+        paragrafo.innerHTML = "Nome............: ".concat(this.nome, " <br>\n                               Modelo..........: ").concat(this.modelo, " <br>\n                               Serie..............: ").concat(this.serie, " <br>\n                               Maquinista...: ").concat(this.maquinista);
     };
     return Maquina;
 }());
 // Capturando o evento de envio do formulário
-var formulario = document.getElementById('meuFormulario'); // Obtém o elemento do formulário com a tipagem correta
+var formulario = document.getElementById('meuFormulario');
+// Obtém o elemento do formulário com a tipagem correta
 formulario.addEventListener('submit', function (ev) {
     ev.preventDefault(); //evita que minha pagina recaregue
     var inputElemnt = document.getElementById("nomeInput");
@@ -26,16 +27,22 @@ formulario.addEventListener('submit', function (ev) {
     var valorImput4 = inputElemnt4.value;
     var maquina1 = new Maquina(valorImput, valorImput2, Number(valorImput3), valorImput4);
     maquina1.apresentar();
-    limpaForm();
+    limpaForm(formulario);
     return maquina1;
 });
-function limpaForm() {
-    var inputElemnt = document.getElementById("nomeInput");
-    var valorImput = inputElemnt.value = "";
-    var inputElemnt2 = document.getElementById("modelInput");
-    var valorImput2 = inputElemnt2.value = "";
-    var inputElemnt3 = document.getElementById("serieInput");
-    var valorImput3 = inputElemnt3.value = "";
-    var inputElemnt4 = document.getElementById("maquinista");
-    var valorImput4 = inputElemnt4.value = "";
+//2 maneiras de lipa o formulario
+function limpaForm(formulario) {
+    formulario.reset();
 }
+/*
+function limpaForm(){
+    const inputElemnt = document.getElementById("nomeInput") as HTMLInputElement
+const valorImput = inputElemnt.value = "";
+const inputElemnt2 = document.getElementById("modelInput") as HTMLInputElement
+const valorImput2 = inputElemnt2.value = "";
+const inputElemnt3 = document.getElementById("serieInput") as HTMLInputElement
+const valorImput3 = inputElemnt3.value = "";
+const inputElemnt4 = document.getElementById("maquinista") as HTMLInputElement
+const valorImput4 = inputElemnt4.value = "";
+}
+*/
--- 00-EXERCICIOS/Exer_Objeto/001/src/index.ts
+++ 00-EXERCICIOS/Exer_Objeto/001/src/index.ts
@@ -11,17 +11,18 @@
         this.serie = serie;
         this.maquinista = maquinista
     }
-    apresentar():void{
-        const paragrafo = document.getElementById("result") as HTMLAnchorElement
-  paragrafo.innerHTML = `Nome..: ${ this.nome } -->
-  Modelo..: ${ this.modelo } -->
-  Serie...: ${ this.serie } -->
-  Maquinista..: ${ this.maquinista}`
-    }
+    apresentar(): void {
+        const paragrafo = document.getElementById("result") as HTMLParagraphElement;
+        paragrafo.innerHTML = `Nome............: ${this.nome} <br>
+                               Modelo..........: ${this.modelo} <br>
+                               Serie..............: ${this.serie} <br>
+                               Maquinista...: ${this.maquinista}`;
+      }
 }
 
 // Capturando o evento de envio do formulário
-const formulario = document.getElementById('meuFormulario') as HTMLFormElement; // Obtém o elemento do formulário com a tipagem correta
+const formulario = document.getElementById('meuFormulario') as HTMLFormElement; 
+// Obtém o elemento do formulário com a tipagem correta
 
 formulario.addEventListener('submit',(ev)=>{
     ev.preventDefault()//evita que minha pagina recaregue
@@ -36,13 +37,19 @@ const valorImput4 = inputElemnt4.value;
 
 const maquina1 = new Maquina(valorImput,valorImput2,Number(valorImput3),valorImput4)
 maquina1.apresentar()
-limpaForm()
+limpaForm(formulario)
 
 
 return maquina1
 })
 
+//2 maneiras de lipa o formulario
 
+
+function limpaForm(formulario: HTMLFormElement): void {
+    formulario.reset();
+  }
+/*
 function limpaForm(){
     const inputElemnt = document.getElementById("nomeInput") as HTMLInputElement
 const valorImput = inputElemnt.value = "";
@@ -53,5 +60,5 @@ const valorImput3 = inputElemnt3.value = "";
 const inputElemnt4 = document.getElementById("maquinista") as HTMLInputElement
 const valorImput4 = inputElemnt4.value = "";
 }
-
+*/
 
