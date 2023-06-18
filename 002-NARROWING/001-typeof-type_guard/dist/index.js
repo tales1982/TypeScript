9e2bf402c086fa1466b6d3875a2cf8f2ab309f82
tales1982 <tales_lima_1982@hotmail.com>
2023-06-18 19:40:26 +0200

    type guard

---
+++ 002-NARROWING/001-typeof-type_guard/dist/index.js
@@ -0,0 +1,15 @@
+//1 type guard
+function sum(a, b) {
+    if (typeof a === "string" && typeof b === "string") {
+        console.log(parseFloat(a) + parseFloat(b));
+    }
+    else if (typeof a === "number" && typeof b === "number") {
+        console.log(a + b);
+    }
+    else {
+        console.log("Imposivel de realitar a soma!");
+    }
+}
+sum(10, 10);
+sum("10", "10");
+sum("10", 50);
