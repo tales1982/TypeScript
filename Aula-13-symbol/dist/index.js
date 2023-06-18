e1dbe521fa474877065493464770d5c09ab2bdb5
tales1982 <tales_lima_1982@hotmail.com>
2023-06-18 18:26:36 +0200

    Symbol

---
+++ Aula-13-symbol/dist/index.js
@@ -0,0 +1,12 @@
+// Definindo um símbolo
+const mySymbol = Symbol("mySymbol");
+// Usando um símbolo como chave de propriedade de objeto
+const obj = {
+    [mySymbol]: "Valor do símbolo"
+};
+console.log(obj[mySymbol]); // Valor do símbolo
+// Iterando sobre as propriedades de um objeto, incluindo os símbolos
+const symbols = Object.getOwnPropertySymbols(obj);
+symbols.forEach(symbol => {
+    console.log(obj[symbol]); // Valor do símbolo
+});
