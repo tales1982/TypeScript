cff1d2b067e860da3d9174be0d752942eac8c01c
tales1982 <tales_lima_1982@hotmail.com>
2023-06-18 22:23:24 +0200

    Checando se o valor existe

---
+++ 002-NARROWING/002-checando_valores_existe/src/index.ts
@@ -0,0 +1,18 @@
+//checando se o valor exister
+//usando o operdo ? o segundo argumento se torna opcional
+function operations(arr: number[],operation?: string | undefined){
+    if(operation){
+        if(operation === "sum"){
+            const sum = arr.reduce((i,total)=>i + total)
+            console.log(sum)
+        }else if(operation === "multiply"){
+            const multiply = arr.reduce((i,total)=>i * total)
+            console.log(multiply)
+        }
+    }else{
+        console.log('Porfavor, defina uma operaçao')
+    }
+}
+
+operations([2,5,8],"multiply")
+operations([5,10,20])
