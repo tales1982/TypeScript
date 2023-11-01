71aab73d9f903be2dc3c6b9d432d95c1d1f2ca2f
Tales Lima <142450583+TalesPortfolio@users.noreply.github.com>
2023-11-01 10:38:08 +0100

    exercicio_ts

---
+++ 00-EXERCICIOS/exercicio_ts/main.js
@@ -0,0 +1,4 @@
+var multiplicacao = function (arg1, arg2) { return arg1 * arg2; };
+console.log(multiplicacao(5, 4));
+var hello = function (name) { return "Ola ".concat(name, " seja bem vindo!"); };
+console.log(hello('Tales'));
---
+++ 00-EXERCICIOS/exercicio_ts/main.ts
@@ -0,0 +1,6 @@
+const multiplicacao = (arg1:number,arg2:number):number => arg1 * arg2;
+console.log(multiplicacao(5,4));
+
+
+const hello = (name:string):string => `Ola ${name} seja bem vindo!`;
+console.log(hello('Tales'));
